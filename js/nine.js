db1396989ea0503f9305ad7593405fbe30bbfb4d
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2024-11-27 10:53:45 +0500

    some changes added

--- js/nine.js
+++ js/nine.js
@@ -1,14 +1,9 @@
-// let d = 0;
-// let arr = [10, 20, 40];
-// for (let i = 0; i < arr.length; i++) {
-//   const element = arr[i];
-//   d += arr[i];
-// }
+let d = 0;
+let arr = [10, 20, 40];
+for (let i = 0; i < arr.length; i++) {
+  const element = arr[i];
+  d += arr[i];
+}
 
-// console.log(Math.round(d / arr.length));
-let son = confirm("son kiriting");
-let ason = prompt("1 - son");
-let bson = prompt("2 - son");
-let cson = prompt("3 - son");
-umumiy = +ason + bson + cson;
-console.log(umumiy);
+console.log(Math.round(d / arr.length));
+// ustoz shu masalni n14 dagi bolladan yordam soradim tushunmagandim qolgan hammasini o'zim qildim)
