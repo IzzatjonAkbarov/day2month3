977516b8018bc054fe825ddf2fefd93ed24ebe40
IzzatjonAkbarov <akbrv.i.006@gmail.com>
2024-11-27 12:37:11 +0500

    some changes added

--- js/nine.js
+++ js/nine.js
@@ -1,9 +1,20 @@
-let d = 0;
-let arr = [10, 20, 40];
-for (let i = 0; i < arr.length; i++) {
-  const element = arr[i];
-  d += arr[i];
-}
+// let d = 0;
+// let arr = [10, 20, 40];
+// for (let i = 0; i < arr.length; i++) {
+//   const element = arr[i];
+//   d += arr[i];
+// }
 
-console.log(Math.round(d / arr.length));
-// ustoz shu masalni n14 dagi bolladan yordam soradim tushunmagandim qolgan hammasini o'zim qildim)
+// console.log(Math.round(d / arr.length));
+// bunisini n14 dan yordam soradm
+let son = confirm("son kiriting");
+let ason = prompt("1 - son");
+let num1 = Number(ason);
+let bson = prompt("2 - son");
+let num2 = Number(bson);
+
+let cson = prompt("3 - son");
+let num3 = Number(cson);
+
+console.log((umumiy = num1 + num2 + num3) / 3);
+// bunisi ozim qildim
